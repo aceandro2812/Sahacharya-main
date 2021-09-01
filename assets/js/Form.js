cb8ecdfb5d30d97e61e865a22d4f21cf828f9334
Jatin Iyer <jatin096@gmail.com>
2021-09-01 14:42:30 +0530

    new updates

---
+++ assets/js/Form.js
@@ -0,0 +1,37 @@
+
+    $(`#success`).hide();
+    $(`#error`).hide();
+
+
+// $("#form").submit(function (){
+//     let name = $(`#name`).val();
+//     let email = $(`#email`).val();
+//     let message = $(`#message`).val();
+//     let subject = $(`#subject`).val();
+//     alert("inside ajax");
+//     $.ajax({
+//         method: 'POST',
+//         url: 'https://formsubmit.co/ajax/sahacharyasms@gmail.com',
+//         dataType: 'json',
+//         accepts: 'application/json',
+//         data: {
+//             name: name,
+//             email: email,
+//             subject: subject,
+//             message: message
+
+//         },
+    
+//         success: function (data){
+//             console.log(data)
+//             alert("success");
+        
+//         },
+//         error: function (err){
+//             console.log(err)
+//             alert("Error");
+//           }
+//     });
+   
+
+// });
